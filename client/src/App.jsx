1aa3738a2d045a466af8270806c3e9b40c74be6c
Miles Eng <miles.eng@duke.edu>
2023-12-17 14:55:52 -0800

    iniitalized client structure

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+import Navbar from './pages/Navbar'
+
+function App() {
+
+  return (
+    <div className='h-screen'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
