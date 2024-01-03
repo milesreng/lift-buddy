493cad85cbcb276e11ee6a6105c62a445549bd1a
milesreng <miles.eng21@gmail.com>
2024-01-03 10:04:51 -0800

    updated checkauth for postman authentication

--- server/middleware/checkAuth.js
+++ server/middleware/checkAuth.js
@@ -2,11 +2,12 @@ const jwt = require('jsonwebtoken')
 
 function checkAuth(req, res, next) {
   try {
-    const token = req.headers['x-access-token']
+    const token = req.headers['authorization'].split(' ')[1]
     const decodedToken = jwt.verify(token, process.env.SECRET_KEY)
     req.userData = { userId: decodedToken.userId, email: decodedToken.email }
     next()
   } catch (e) {
+    console.log(e)
     return res.status(401).json({ error: 'user authentication failed' })
   }
 }
