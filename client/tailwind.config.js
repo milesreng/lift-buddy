96924ce74280f8fdf80d3312251582a664d81f77
Miles Eng <miles.eng@duke.edu>
2024-01-10 21:30:21 -0500

    updated packages

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,24 @@ export default {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'palette': {
+          'dark': '#040D12',
+          'dark-accent': '#183D3D',
+          'mid-accent': '#497061',
+          'mid': '#5C8374',
+          'light-accent': '#93B1A6',
+          'light': '#c3d6ce',
+          'lightest': '#e1ebe7',
+          'bg': '#e5ebe9'
+        }
+      },
+      fontFamily: {
+        'content': ['Cabin', 'sans-serif'],
+        'sketch': ['Cabin Sketch', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
